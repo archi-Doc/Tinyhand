17c183974715e1a518c8bed4d23d21d94bd7e37c
archi-Doc <archidoc422@gmail.com>
2020-11-10 15:51:22 +0900

    Generator issue

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp1
         }
     }
 
-    [TinyhandGeneratorOption(AttachDebugger = true, GenerateToFile = true)]
+    // [TinyhandGeneratorOption(AttachDebugger = true, GenerateToFile = true)]
     class Program
     {
         static void Main(string[] args)
