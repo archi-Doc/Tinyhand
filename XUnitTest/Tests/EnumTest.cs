a390f5c74f663a3beb16395317c21ac800691100
archi-Doc <archidoc422@gmail.com>
2021-05-14 21:47:40 +0900

    Fix handling multiple enum instances

---
+++ XUnitTest/Tests/EnumTest.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tinyhand;
+using Xunit;
+
+namespace Tinyhand.Tests
+{
+    public enum MyEnum { One, Two, Three }
+
+    [TinyhandObject]
+    public partial class EnumTestClass
+    {
+        [Key(1)]
+        public MyEnum Item1 { get; set; }
+
+        [Key(2)]
+        public MyEnum Item2 { get; set; }
+
+        [Key(3)]
+        public MyEnum Item3 { get; set; }
+
+    }
+
+    public class EnumTest
+    {
+        [Fact]
+        public void MultipleEnumTest()
+        {
+            var t = new EnumTestClass();
+            t.Item2 = MyEnum.Two;
+            t.Item3 = MyEnum.Three;
+            var t2 = TestHelper.Convert(t);
+            t2.IsStructuralEqual(t);
+        }
+    }
+}
