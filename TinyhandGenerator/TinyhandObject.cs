ab3dcb06d6b143541adeccf135a7e55d91d7ead4
archi-Doc <archidoc422@gmail.com>
2021-05-21 21:46:55 +0900

    .

--- TinyhandGenerator/TinyhandObject.cs
+++ TinyhandGenerator/TinyhandObject.cs
@@ -1404,7 +1404,8 @@ ModuleInitializerClass_Added:
                 {
                     ssb.AppendLine($"mi = type.GetMethod(\"set_{x.SimpleName}\")!;");
                     ssb.AppendLine($"exp = Expression.Parameter(typeof({x.TypeObject!.FullName}));");
-                    ssb.AppendLine($"{x.InitDelegateIdentifier} = Expression.Lambda<Action<{this.LocalName}, {x.TypeObject!.FullName}>>(Expression.Call(expType, mi!, exp), expType, exp).Compile();");
+                    // ssb.AppendLine($"{x.InitDelegateIdentifier} = Expression.Lambda<Action<{this.LocalName}, {x.TypeObject!.FullName}>>(Expression.Call(expType, mi!, exp), expType, exp).Compile();");
+                    ssb.AppendLine($"{x.InitDelegateIdentifier} = FastExpressionCompiler.ExpressionCompiler.CompileFast(Expression.Lambda<Action<{this.LocalName}, {x.TypeObject!.FullName}>>(Expression.Call(expType, mi!, exp), expType, exp));");
                 }
 
                 ssb.AppendLine("return true;");
