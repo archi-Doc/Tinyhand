3450725323c4c3a637807e6db3803c309b548640
archi-Doc <archidoc422@gmail.com>
2022-09-27 21:39:01 +0900

    .

--- Tinyhand/Misc/KeyValueList.cs
+++ Tinyhand/Misc/KeyValueList.cs
@@ -5,6 +5,12 @@ using System.Collections.Generic;
 
 namespace Tinyhand;
 
+/// <summary>
+/// A collection of key value pairs (List&lt;<see cref="KeyValuePair{TKey, TValue}"/>&gt;).<br/>
+/// Represents a <see cref="KeyValuePair{TKey, TValue}"/> as a map structure (key = { value }).
+/// </summary>
+/// <typeparam name="TKey">The type of the key. </typeparam>
+/// <typeparam name="TValue">The type of the value.</typeparam>
 public class KeyValueList<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
 {
     public KeyValueList()
