faf5560a0a42e9435dc46385c438fdb55b2dd80b
archi-Doc <archidoc422@gmail.com>
2023-05-13 08:38:29 +0900

    .

--- SandboxBase/LockObjectClass.cs
+++ SandboxBase/LockObjectClass.cs
@@ -12,11 +12,14 @@ public partial class LockObjectClassA
     protected readonly SemaphoreLock semaphore = new();
 }
 
-[TinyhandObject(LockObject = "semaphore")]
+[TinyhandObject(LockObject = "semaphore", ExplicitKeyOnly = true)]
 public partial class PropertyTestClass
 {
     [Key(0, PropertyName = "Id")]
     public int id;
+
+    // [IgnoreMember]
+    public string Name = string.Empty;
 
     protected readonly SemaphoreLock semaphore = new();
 }
