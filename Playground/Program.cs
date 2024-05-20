0e4e50f650c5a95397a66c7654f20ed3fe4f3a14
archi-Doc <archidoc422@gmail.com>
2024-05-21 00:07:18 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -2,47 +2,6 @@
 
 namespace Playground;
 
-public class IntegralityEngine
-{
-    public IntegralityEngine()
-    {
-    }
-
-    public int Level { get; private set; }
-
-    public void GetIdentity(IIntegrality integrality)
-    {
-        if (this.Level == 0)
-        {// Get root hash
-            var rootHash = integrality.GetRootHash();
-        }
-    }
-}
-
-public class IntegralityContext
-{
-}
-
-public class Test1
-{
-    public void Test()
-    {
-        var engine = new IntegralityEngine();
-        /*var identity = engine.GetIdentity(g);
-        var difference = engine.GetDifference();
-        engine.Integrate(difference);*/
-    }
-}
-
-public interface IIntegrality
-{// Exaltation of the Integrality by Baxter.
-    ulong RootHash { get; set; }
-
-    ulong GetRootHash();
-
-    void ClearRootHash();
-}
-
 internal class Program
 {
     static void Main(string[] args)
