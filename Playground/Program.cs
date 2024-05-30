0e29492c438119faaad3ed6234b885531f470357
archi-Doc <archidoc422@gmail.com>
2024-05-30 22:02:23 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,8 +1,33 @@
 ﻿using System;
 using Arc.Collections;
+using Tinyhand;
 using Tinyhand.IO;
+using ValueLink;
 
 namespace Playground;
+
+[TinyhandObject]
+[ValueLinkObject(Integrality = true)]
+public partial class GenericIntegralityClass2<T>
+    where T : ITinyhandSerialize<T>
+{
+    [Link(Primary = true, Unique = true, Type = ChainType.Unordered, TargetMember = "Id2")]
+    public GenericIntegralityClass2()
+    {
+    }
+
+    public int Id2 => this.id2;
+
+    [Key(1)]
+    public T Value { get; set; } = default!;
+
+    [Key(0)]
+    private int id2;
+
+    [Key(2, AddProperty = "Name")]
+    private string name = string.Empty;//
+}
+
 
 internal class Program
 {
