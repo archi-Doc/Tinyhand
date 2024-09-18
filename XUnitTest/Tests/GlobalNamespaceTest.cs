af88552222afbc74e02c2dbaacbd3aa3e08567ee
archi-Doc <archidoc422@gmail.com>
2024-09-18 17:49:52 +0900

    .

--- XUnitTest/Tests/GlobalNamespaceTest.cs
+++ XUnitTest/Tests/GlobalNamespaceTest.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-namespace Tinyhand.Tests;
+using Tinyhand;
 
 [TinyhandObject]
 public partial class GlobalNamespaceTestClass
