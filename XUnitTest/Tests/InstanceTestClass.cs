223ea90b3c1cf3c581971d7a4f91ca27de659c66
archi-Doc <archidoc422@gmail.com>
2024-12-24 12:48:50 +0900

    .

---
+++ XUnitTest/Tests/InstanceTestClass.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tinyhand;
+using Xunit;
+
+namespace XUnitTest.Tests;
+
+[TinyhandObject]
+public partial class InstanceTestClass
+{
+    public InstanceTestClass()
+    {
+    }
+
+    [Key(0)]
+    public Dictionary<string, string> Dictionary { get; set; } = new(StringComparer.InvariantCultureIgnoreCase);
+}
+
+public class InstanceTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var tc = new InstanceTestClass();
+        tc.Dictionary.Add("A", "1");
+        tc.Dictionary.ContainsKey("A").IsTrue();
+        tc.Dictionary.ContainsKey("a").IsTrue();
+
+        tc = TinyhandSerializer.Deserialize<InstanceTestClass>(TinyhandSerializer.Serialize(tc));
+        tc.Dictionary.ContainsKey("A").IsTrue();
+        tc.Dictionary.ContainsKey("a").IsTrue();
+    }
+}
