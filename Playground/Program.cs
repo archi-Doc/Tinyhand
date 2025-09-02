bb4b1c064060674d970d2094e6d809278ddca515
archi-Doc <archidoc422@gmail.com>
2025-09-02 14:30:08 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,16 +1,73 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-using Arc.Collections.HotMethod;
 using CrystalData;
 using Tinyhand;
 using Tinyhand.IO;
-using ValueLink;
 
 namespace Playground;
 
 [TinyhandObject]
 public partial class PartialImplementationClass
 {
+    /*public sealed class ReadOnly : PartialImplementationClass
+    {
+        public void Test()
+        {
+            var a = new ReadOnly();
+            a.SerializeToRentMemory();
+            TinyhandSerializer.TryDeserializeObject<PartialImplementationClass>([], out var c!);
+            a = (ReadOnly)c;
+        }
+
+        public new string Name => base.Name;
+    }*/
+
+    public sealed class Immutable : ITinyhandSerializable<Immutable>, ITinyhandReconstructable<Immutable>, ITinyhandCloneable<Immutable>
+    {
+        private readonly PartialImplementationClass underlyingObject;
+
+        public Immutable(PartialImplementationClass obj)
+        {
+            this.underlyingObject = obj;
+        }
+
+        public PartialImplementationClass GetUnderlyingObject() => this.underlyingObject;
+
+        /*static void ITinyhandSerializable<ReadOnly2>.Serialize(ref TinyhandWriter writer, scoped ref ReadOnly2? value, TinyhandSerializerOptions options)
+        {
+            TinyhandSerializer.SerializeObject(ref writer, in value.underlyingObject, options);
+        }*/
+
+        static void ITinyhandSerializable<Immutable>.Serialize(ref TinyhandWriter writer, scoped ref Immutable? value, TinyhandSerializerOptions options)
+        {
+            if (value is null) writer.WriteNil();
+            else TinyhandSerializer.SerializeObject(ref writer, in value.underlyingObject, options);
+        }
+
+        static void ITinyhandSerializable<Immutable>.Deserialize(ref TinyhandReader reader, scoped ref Immutable? value, TinyhandSerializerOptions options)
+        {
+            if (TinyhandSerializer.DeserializeObject<PartialImplementationClass>(ref reader, options) is { } obj) value = new(obj);
+        }
+
+        static Immutable? ITinyhandCloneable<Immutable>.Clone(scoped ref Immutable? value, TinyhandSerializerOptions options)
+        {
+            if (TinyhandSerializer.CloneObject(value?.underlyingObject, options) is { } obj) return new(obj);
+            else return default;
+        }
+
+        static void ITinyhandReconstructable<Immutable>.Reconstruct([NotNull] scoped ref Immutable? value, TinyhandSerializerOptions options)
+        {
+            value ??= new(TinyhandSerializer.ReconstructObject<PartialImplementationClass>(options));
+        }
+
+        public string Name => this.underlyingObject.Name;
+    }
+
+    public Immutable ToImmutable() => new(this);
+
+    public Immutable CloneAndToImmutable() => new(TinyhandSerializer.CloneObject(this));
+
+
     public PartialImplementationClass()
     {
     }
