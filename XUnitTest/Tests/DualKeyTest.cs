b4e41986bcbde0e844aad2d7f38046684a3d4a91
archi-Doc <archidoc422@gmail.com>
2025-10-04 22:19:39 +0900

    .

--- XUnitTest/Tests/DualKeyTest.cs
+++ XUnitTest/Tests/DualKeyTest.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Text;
 using Tinyhand;
+using Tinyhand.Tests;
 using Xunit;
 
 namespace XUnitTest.Tests;
@@ -15,6 +16,9 @@ public partial class DualKeyTestClass : IEquatable<DualKeyTestClass>
 
     [Key(1)]
     public string B { get; set; } = "Test";
+
+    [Key(2)]
+    public StringConvertibleTestClass Class1 { get; set; } = new();
 
     public DualKeyTestClass()
     {
@@ -36,7 +40,9 @@ public class DualKeyTest
     [Fact]
     public void Test1()
     {
-        var tc = new DualKeyTestClass() { A = 123, B = "Hello" };
+        var td = new StringConvertibleTestClass();
+        td.Byte16 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,];
+        var tc = new DualKeyTestClass() { A = 123, B = "Hello", Class1 = td, };
 
         var bin = TinyhandSerializer.Serialize(tc);
         var tc2 = TinyhandSerializer.Deserialize<DualKeyTestClass>(bin);
@@ -47,11 +53,11 @@ public class DualKeyTest
         tc2.Equals(tc).IsTrue();
 
         tc = new() { A = 1 };
-        tc2 = TinyhandSerializer.DeserializeFromString<DualKeyTestClass>("A = 1");
+        tc2 = TinyhandSerializer.DeserializeFromString<DualKeyTestClass>("A = 1,Class1=\"@AQIDBAUGBwgJCgsMDQ4PEA\"");
         tc2.Equals(tc).IsTrue();
 
         var strictOption = TinyhandSerializerOptions.ConvertToString with { Compose = TinyhandComposeOption.Strict, };
-        tc2 = TinyhandSerializer.DeserializeFromString<DualKeyTestClass>("{A = 1}", strictOption);
+        tc2 = TinyhandSerializer.DeserializeFromString<DualKeyTestClass>("{A = 1, Class1=\"@AQIDBAUGBwgJCgsMDQ4PEA\"}", strictOption);
         tc2.Equals(tc).IsTrue();
     }
 }
